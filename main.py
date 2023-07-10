a553caa725a9cdd81106bbb33b7c66386c6db975
Artem Luchka <artemlucka@gmail.com>
2023-07-10 19:36:11 +0200

    wrote in data.json

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import json
 def f():
+    # Данные которые добавляем в json
+    data = {
+        "user_name": "Anton",
+        "user_age": 35,
+        "user_email": "aaa@gmail.com"
+    }
     # Создания файла Json
     with open("data.json", "w") as f:
-        pass
-        pass
+        json.dump(data, f)
 f()
