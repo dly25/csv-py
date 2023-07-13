512a3ec7c3e9e15e26a3fa443645fe521edf1224
Artem Luchka <artemlucka@gmail.com>
2023-07-13 18:48:05 +0200

    added change for name

--- main.py
+++ main.py
@@ -9,7 +9,20 @@ def f():
     # Создания файла Json
     with open("data.json", "w", encoding="utf-8") as file:
         file.write(json.dumps(data, indent=4, ensure_ascii=False))
+
     with open("data.json", encoding="utf-8") as file:
-        a = json.load(file)
-    print(a)
+        data = json.load(file)
+        print(data)
+
+
+    data["user_name"] = "Artem"
+
+    with open("data.json", "w") as file:
+        file.write(json.dumps(data, indent=4, ensure_ascii=False))
+
+    with open("data.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+        print(data)
+
+
 f()
