f060dce3c3e928f8bc5491e56fc8effffa8653e4
Artem Luchka <artemlucka@gmail.com>
2023-07-14 17:48:48 +0200

    added coms

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def f():
     # Создания файла Json
     with open("data.json", "w", encoding="utf-8") as file:
         file.write(json.dumps(data, indent=4, ensure_ascii=False))
-
+    # Показ данных JSON файла
     with open("data.json", encoding="utf-8") as file:
         data = json.load(file)
         print(data)
 
-
+    # Изменения JSON файла
     data["user_name"] = "Artem"
 
     with open("data.json", "w") as file:
@@ -23,6 +23,4 @@ def f():
     with open("data.json", "r", encoding="utf-8") as file:
         data = json.load(file)
         print(data)
-
-
 f()
