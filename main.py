9789574afccd9225fd01f7ae2b1c05849e8bc4c2
Artem Luchka <artemlucka@gmail.com>
2023-07-16 19:28:21 +0200

    new

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import json
 def f():
     # Данные которые добавляем в json
     data = {
-        "user_name": "Anton",
-        "user_age": 35,
-        "user_email": "aaa@gmail.com"
+        "person1": {
+            "user_name": "Anton",
+            "user_age": 35,
+            "user_email": "aaa@gmail.com"
+        },
+        "person2": {
+            "user_name": "Anton",
+            "user_age": 35,
+            "user_email": "aaa@gmail.com"
+        }
     }
     # Создания файла Json
     with open("data.json", "w", encoding="utf-8") as file:
@@ -15,7 +22,7 @@ def f():
         print(data)
 
     # Изменения JSON файла
-    data["user_name"] = "Artem"
+    data[list(data.keys())[1]]["user_name"] = "Artem"
 
     with open("data.json", "w") as file:
         file.write(json.dumps(data, indent=4, ensure_ascii=False))
@@ -23,4 +30,11 @@ def f():
     with open("data.json", "r", encoding="utf-8") as file:
         data = json.load(file)
         print(data)
+    def filter_name():
+        # Фильтрация данных и вывод определенных полей (например, только имен пользователей)
+        for name in data:
+            user_name = data[name].get('user_name')
+            if user_name:
+                print(user_name)
+    filter_name()
 f()
