11d2f302c7a1f7cb243885699ebe69452c794204
Artem Luchka <artemlucka@gmail.com>
2023-07-19 17:55:17 +0200

    finish

--- main.py
+++ main.py
@@ -58,9 +58,9 @@ def filter_data(data):
                     terminal_data()
                 else:
                     raise ValueError("НЕ правильный запрос!")
-            terminal_data()
         else:
             print()
+            terminal_data()
     except ValueError as e:
         print(e)
     except KeyError:
@@ -96,22 +96,24 @@ def age_data(data):
 def terminal_data():
     data = load_data()
     save_data(data)
+    try:
+        inp = input("Введите команду: ")
 
-    inp = input("Введите команду: ")
-
-    if inp == "show":
-        show_data(data)
-    elif inp == "change":
-        change_data(data)
-    elif inp == "filter":
-        filter_data(data)
-    elif inp == "count":
-        count_data(data)
-    elif inp == "age":
-        age_data(data)
-    else:
-        print("Ошибка!!! Не правильный ввод команды")
-        terminal_data()
+        if inp == "show":
+            show_data(data)
+        elif inp == "change":
+            change_data(data)
+        elif inp == "filter":
+            filter_data(data)
+        elif inp == "count":
+            count_data(data)
+        elif inp == "age":
+            age_data(data)
+        else:
+            print("Ошибка!!! Не правильный ввод команды")
+            terminal_data()
+    except KeyboardInterrupt:
+        print()
 
 def main():
     terminal_data()
