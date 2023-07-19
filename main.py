d2e0c1c070cb162afa6efda35d7f5c0f7a4cc2b2
Artem Luchka <artemlucka@gmail.com>
2023-07-19 15:08:19 +0200

    added finish change

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ def load_data():
     try:
         with open("data.json", "r", encoding="utf-8") as file:
             data = json.load(file)
+            return data
     except FileNotFoundError:
         print("Не найден файл!")
+        return {}
     except json.JSONDecodeError:
         print("Что-то не правильно в файле!")
+        return {}
 
 def save_data(data):
     with open("data.json", "w", encoding="utf-8") as file:
@@ -26,14 +29,50 @@ def show_data(data):
 
 def change_data(data):
     try:
-        inp_person, inp_item, inp_change = input("Введите запрос на изменения data пример(person1)(user_name)(изменения) ").strip().split()
-        data[inp_person][inp_item] = inp_change
-        save_data(data)
-        show_data(data)
+        inp = input("Хотите изменить файл?(yes/no): ")
+        if not inp == "no":
+            inp_person, inp_item, inp_change = input("Введите запрос на изменения data пример(person1)"
+                                                     "(user_name)(изменения) ").strip().split()
+            data[inp_person][inp_item] = inp_change
+            save_data(data)
+            show_data(data)
+        else:
+            print()
     except ValueError:
         print("НЕ правильный запрос!")
     except KeyError:
         print("НЕ правильный ключ!")
+
+def filter_data(data):
+    try:
+        inp = input("Хотите отфильтровать файл?(yes/no): ")
+        if not inp == "no":
+            inp_filter = input("Введите хотите отфильтровать: ")
+            for output in data:
+                output_name = data[output].get(inp_filter)
+                if output_name:
+                    print(output_name)
+                else:
+                    raise ValueError("НЕ правильный запрос!")
+            show_data(data)
+        else:
+            print()
+    except ValueError as e:
+        print(e)
+    except KeyError:
+        print("НЕ правильный ключ!")
+
+def count_data(data):
+    try:
+        inp = input("Хотите count файл?(yes/no): ")
+        if not inp == "no":
+            inp_count = input("Введите хотите count: ")
+            result = len(data[inp_count])
+            print(result)
+        else:
+            print()
+    except ValueError as e:
+        print(e)
 
 def main():
     data = load_data()
@@ -41,6 +80,8 @@ def main():
     save_data(data)
     show_data(data)
     change_data(data)
+    filter_data(data)
+    count_data(data)
 
 if __name__ == "__main__":
     main()
