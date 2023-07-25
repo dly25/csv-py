b51420d054beb65e3a2214fda98c0b85d9afbade
Artem Luchka <artemlucka@gmail.com>
2023-07-26 01:25:53 +0200

    C:/Program Files/Git/

--- csv_module/load_save.py
+++ csv_module/load_save.py
@@ -1,8 +1,11 @@
 import csv
 def load_data():
     try:
-
+        with open("data.csv") as file:
+            data = file.writer()
+            data
     except FileNotFoundError:
         print("Файл не найден!")
-    except сsv
+    except csv.Error:
+        print("Что-то не так с файлом!")
 
